3dc178abb9aebae365d98b5556317f39d9acffbc
Kaitlyn <katiefujihara@gmail.com>
2019-02-25 10:33:13 -0800

    add site description

--- config/website.js
+++ config/website.js
@@ -6,7 +6,7 @@ module.exports = {
   siteUrl: 'https://katiefujihara.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
+  siteDescription: 'UX Engineer in Portland, OR',
   author: 'Author', // Author for schemaORGJSONLD
   organization: 'Katie Fujihara',
 
